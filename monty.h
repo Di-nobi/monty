d96b0bedbafa0ca24a37ea39a77c678045620cf5
Dinobi <architectstan01@gmail.com>
2023-01-26 08:18:20 -0800

    updates

--- monty.h
+++ monty.h
@@ -42,7 +42,6 @@ extern bus_t bus;
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
